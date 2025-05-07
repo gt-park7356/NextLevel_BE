2cedbffddd979f32732deedd71a52dc141b32f3f
gt-park7356 <tlals0028@gmail.com>
2025-05-07 15:19:52 +0900

    initial commit

---
+++ src/main/java/com/example/NextLevel/NextLevelApplication.java
@@ -0,0 +1,13 @@
+package com.example.NextLevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextLevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextLevelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NextLevel/NextLevelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NextLevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextLevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
