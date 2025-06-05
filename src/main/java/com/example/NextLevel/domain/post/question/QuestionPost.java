d3d1dbe4a5a394618c0aaaf463680edcc556d267
gt-park7356 <tlals0028@gmail.com>
2025-06-06 08:16:02 +0900

    refactor: 필요없는 클래스 삭제

--- src/main/java/com/example/NextLevel/domain/post/question/QuestionPost.java
+++
@@ -1,4 +0,0 @@
-package com.example.NextLevel.domain.post.question;
-
-public class QuestionPost {
-}
--- src/main/java/com/example/NextLevel/domain/post/team/TeamMember.java
+++
@@ -1,4 +0,0 @@
-package com.example.NextLevel.domain.post.team;
-
-public class TeamMember {
-}
